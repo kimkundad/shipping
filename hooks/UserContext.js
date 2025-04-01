fce87027403869d61acbf04299adf97d6cbb7582
kimkundad <kim.kundad@gmail.com>
2025-04-01 15:14:59 +0700

    kim

--- hooks/UserContext.js
+++ hooks/UserContext.js
@@ -15,30 +15,32 @@ export const UserProvider = ({ children }) => {
     const fetchUserProfile = async () => {
       try {
         const token = await AsyncStorage.getItem('jwt_token');
+        //console.log('Retrieved token:', token); // Debug token retrieval
         const dataUser = await AsyncStorage.getItem('user_profile');
-  
+        //console.log('Retrieved user profile:', dataUser);
+
         if (dataUser) {
           setUserProfile(JSON.parse(dataUser));
         }
-  
+
         const ordersResponse = await api.get('/user-order');
-        setUserOrders(ordersResponse.data.order);
-  
+        if (ordersResponse) {
+          setUserOrders(ordersResponse.data.order);
+        }
+
         const branchResponse = await api.get('/user-branch');
-        setUserBranch(branchResponse.data.branch);
-  
+      //  console.log('branch response----> 1:', branchResponse.data);
+        if (branchResponse) {
+          setUserBranch(branchResponse.data.branch);
+        }
+
       } catch (error) {
         console.error('Failed to retrieve user profile:', error);
-        if (error.response?.status === 401) {
-          await AsyncStorage.clear();
-          navigation.navigate('(alogin)'); // หรือ router.replace()
-        }
       }
     };
-  
+
     fetchUserProfile();
   }, []);
-  
 
   const logout = async () => {
     try {
